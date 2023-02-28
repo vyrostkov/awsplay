a949a57f4e1d05a74449f66353657d6cf157f4a3
Dmitry Vyrostkov <vyrostkov@dataart.com>
2023-02-28 16:16:03 +0000

    Terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,13 +72,13 @@ resource "aws_ecs_task_definition" "awsplay" {
   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
   memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
-  execution_role_arn       = "${aws_iam_role.awsplay_task_execution_role.arn}"
-  task_role_arn            = "${aws_iam_role.awsplay_task_role.arn}"
+  execution_role_arn       = aws_iam_role.awsplay_task_execution_role.arn
+  task_role_arn            = aws_iam_role.awsplay_task_role.arn
 }
 
 resource "aws_iam_role" "awsplay_task_execution_role" {
   name               = "awsplay-task-execution-role"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -93,17 +93,17 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "awsplay_task_execution_role_policy" {
-  role       = "${aws_iam_role.awsplay_task_execution_role.name}"
+  role       = aws_iam_role.awsplay_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role" "awsplay_task_role" {
   name               = "awsplay-task-role"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "awsplay_task_role_policy" {
-  role       = "${aws_iam_role.awsplay_task_role.name}"
+  role       = aws_iam_role.awsplay_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
@@ -141,38 +141,38 @@ resource "aws_lb_target_group" "fargate" {
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+  vpc_id      = aws_default_vpc.default_vpc.id
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = "${aws_alb.awsplay_lb.arn}" # Referencing our load balancer
+  load_balancer_arn = aws_alb.awsplay_lb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
-  certificate_arn   = "${var.acm_cert_arn}"
+  certificate_arn   = var.acm_cert_arn
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.fargate.arn}" # Referencing our tagrte group
+    target_group_arn = aws_lb_target_group.fargate.arn
   }
 }
 
 resource "aws_ecs_service" "awsplay_service" {
   name            = "awsplay-service"
-  cluster         = "${aws_ecs_cluster.awsplay.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.awsplay.arn}"    # Referencing the task our service will spin up
+  cluster         = aws_ecs_cluster.awsplay.id
+  task_definition = aws_ecs_task_definition.awsplay.arn
   launch_type     = "FARGATE"
   desired_count   = 1
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.fargate.arn}" # Referencing our target group
-    container_name   = "${aws_ecs_task_definition.awsplay.family}"
+    target_group_arn = aws_lb_target_group.fargate.arn
+    container_name   = aws_ecs_task_definition.awsplay.family
     container_port   = 80 # Specifying the container port
   }
 
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
-    assign_public_ip = true                                                # Providing our containers with public IPs
-    security_groups  = ["${aws_security_group.awsplay_fargate_security_group.id}"] # Setting the security group
+    assign_public_ip = true
+    security_groups  = ["${aws_security_group.awsplay_fargate_security_group.id}"]
   }
 }
 
@@ -195,21 +195,21 @@ resource "aws_security_group" "awsplay_fargate_security_group" {
 }
 
 resource "aws_route53_record" "awsplay" {
-  zone_id = "${var.route53_zone}"
-  name    = "${var.route53_domain}"
+  zone_id = var.route53_zone
+  name    = var.route53_domain
   type    = "A"
 
   alias {
-    name                   = "${aws_alb.awsplay_lb.dns_name}"
-    zone_id                = "${aws_alb.awsplay_lb.zone_id}"
+    name                   = aws_alb.awsplay_lb.dns_name
+    zone_id                = aws_alb.awsplay_lb.zone_id
     evaluate_target_health = true
   }
 }
 
 resource "aws_dynamodb_table" "awsplay" {
-  name           = "awsplay"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+  name         = "awsplay"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
     name = "id"
