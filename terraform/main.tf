4c45e10007383ad1203b31ba940045414890b554
Dmitry Vyrostkov <vyrostkov@dataart.com>
2023-02-28 20:14:48 +0000

    Fix image in terraform script

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,11 +19,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Providing a reference to our default VPC
 resource "aws_default_vpc" "default_vpc" {
 }
 
-# Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "${var.aws_region}a"
 }
